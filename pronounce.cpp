e79c9684ca013c33f87a968be266c981de6cbd53
NSz <nick.szewczak@gmail.com>
2018-10-05 11:51:56 -0400

    moved to phase 2

--- pronounce.cpp
+++ pronounce.cpp
@@ -1,57 +1,60 @@
-/*
 Author: Nick Szewczak
 Course: CSCI-136
 Instructor: Subhadarshi Panda
-Assignment: Project 2 Phase 1
+Assignment: Project 2 phase 2 Pesudocode
+Task:
+    User enters a word — the program reports pronunciation as a list of phonemes AND a list of words that sound the same (is made up of exactly the same sequence of phonemes). Example:
+
+    > donut
+
+    Pronunciation    : D OW1 N AH2 T
+
+    Identical        : DOUGHNUT
+
+~~~~~~~~~~~~~~~~~~~~~
+
+using iostream for cmd in out
+using fstream for file streaming
+using climits for max min opps
+
+streamManager function(string):
+    handed dictionary filename
+    //todo: add overloarding for paths
+    open file stream
+        if fail, send cerr and EXIT
+        else, return stream refrence
+
+searchWordFunction *(string targetWord,stream ref)
+    while ref allows >> word reading:
+        if targetWord = word read from file:
+            return stream ref at current file position
+            break //this skips the exit at end of function for failed search
+        else:
+            skip to end of line to prepare for net read
+    EXIT if while loop concludes and has not returned ref yet, search fail.
+
+searchPrononceFunction *(string targetWord,stream ref)
+    Set streamref to 0 again.
+    string output
+    string temp
+    while ref allows >> word reading:
+        read word to temp
+        readline
+        if pronounce = readline:
+            string output + temp + endl
+    return output string
+
+pronounceFunction (string a, stream ref)
+    assumes stream ref is at start of pronounce string
+    a = readline from streamref
+    return a
+
+main:
+    get cin << target word
+    streamManager(dictionary)
+    searchFunction(targetword)
+    a = pronounceFunction(streamRef)
+    cout << a << endl
+    cout << searchPrononceFunction(a, streamRef)
 
 
-    ifstream fin("Current_Reservoir_Levels.tsv");
-    if (fin.fail()) {
-        cerr << "File cannot be opened for reading." << endl;
-        exit(1); // exit if failed to open the file
-
-    while(fin >> date >> eastSt >> eastEl >> westSt >> westEl){ 
-
-        fin.ignore(INT_MAX, '\n'); //skips to the end of line, 
-
-    fin.close();
-
-
-...*/
-
-#include <iostream>  //cout for debugging
-#include <fstream>   //FileStream access
-#include <climits>   // max min
-
-
-using namespace std;
-
-
-
-
-int main(){
-    ifstream cmu("cmudict.0.7a.txt");
-    if (cmu.fail()) {
-        cerr << "Not found\n";
-        exit(1);
-    }
-    string userword;
-    printf("enter a word:\n");
-    cin >> userword;
-    for(int i=0; i < userword.length(); i++){
-        userword[i]= toupper(userword[i]);
-    }
-
-    string readword;
-    
-    while(cmu >> readword){
-        if(userword==readword){
-            string pronounce;
-            getline(cmu, pronounce);
-            cout << "Pronunciation\t: " << pronounce << endl;
-            break;
-        }
-        else cmu.ignore(INT_MAX, '\n'); //skips to the end of line
-    }
-    return 0;
-}
